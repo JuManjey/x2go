2eef4fd22308b1834e5502edfeaa009079dcb575
JuManji <opghld@gmail.com>
2022-02-15 18:52:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,1 @@
-sudo apt-get update
-sudo apt-get install xubuntu-desktop
-sudo apt-get install x2goserver x2goserver-xsession
-sudo apt-get install x2goclient
+sudo apt-get update && apt-get upgrade && apt-get dist-upgrade && apt autoclean && apt autoremove && pt-get install xubuntu-desktop x2goserver x2goserver-xsession x2goclient
